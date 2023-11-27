e009380928e71ba5f8df25574e5d85cb598bb1fa
DimaKosy <dmytrokosynskyy@gmail.com>
2023-11-27 21:14:33 +0000

    sql functions

--- Nodes/node/app/model/select.js
+++ Nodes/node/app/model/select.js
@@ -1,4 +1,4 @@
-exports.selectData = function(response){
+exports.selectGames = function(response){
 
 	var mysql = require('mysql2');
 
@@ -12,7 +12,7 @@ exports.selectData = function(response){
 
 	con.connect(function(err) {
 	  if (err) throw err;
-	  con.query("SELECT id FROM Education", function (err, result, fields) { // change the query to match your needs and setup
+	  con.query("SELECT game_name FROM games", function (err, result, fields) { // change the query to match your needs and setup
 		if (err) throw err;
 		console.log("From model: " + result);
 		response(result);		
@@ -21,7 +21,7 @@ exports.selectData = function(response){
 	});
 }
 
-exports.selectEdu = function(response){
+exports.selectUsers = function(response){
 
 	var mysql = require('mysql2');
 
@@ -35,7 +35,32 @@ exports.selectEdu = function(response){
 
 	con.connect(function(err) {
 	  if (err) throw err;
-	  con.query("SELECT * FROM Education", function (err, result, fields) { // change the query to match your needs and setup
+	  con.query("SELECT user_name FROM games", function (err, result, fields) { // change the query to match your needs and setup
+		if (err) throw err;
+		console.log("From model: " + result);
+		response(result);		
+		
+	  });
+	});
+}
+
+exports.selectGameList = function(response){
+
+	var mysql = require('mysql2');
+
+	var con = mysql.createConnection({ // change these details to match your installation
+	  host: "localhost",
+	  user: "root",
+	  password: "1234", 
+	  database: "sys",
+	  port:3306
+	});
+
+	con.connect(function(err) {
+	  if (err) throw err;
+	  con.query(
+		"SELECT game_name FROM games join games_list on games_list.game_id = games.game_id where user_id = ?"[userID]
+		, function (err, result, fields) { // change the query to match your needs and setup
 		if (err) throw err;
 		console.log("From model: " + result);
 		response(result);		
