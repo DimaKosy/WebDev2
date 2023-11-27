e4d2ae1e70bc176d1aee04ef9d337b8422a33733
DimaKosy <dmytrokosynskyy@gmail.com>
2023-11-27 22:39:24 +0000

    insert test?

--- Nodes/node/app/model/select.js
+++ Nodes/node/app/model/select.js
@@ -94,7 +94,6 @@ exports.deleteFromGameList = function(response){
 	});
 }
 
-
 exports.updateGameList = function(response){
 
 	var mysql = require('mysql2');
@@ -120,4 +119,26 @@ exports.updateGameList = function(response){
 	});
 }
 
+exports.InsertIntoGamesList = function(response){
+	var mysql = require('mysql2');
 
+	var con = mysql.createConnection({ // change these details to match your installation
+	  host: "localhost",
+	  user: "root",
+	  password: "1234", 
+	  database: "sys",
+	  port:3306
+	});
+
+	con.connect(function(err) {
+	  if (err) throw err;
+	  con.query(
+		"insert into games_list values(?,?);"[game_review, game_Id, user_Id]
+		, function (err, result, fields) { // change the query to match your needs and setup
+		if (err) throw err;
+		console.log("From model: " + result);
+		response(result);		
+		
+	  });
+	});
+}
