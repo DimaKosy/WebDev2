7d91473f09a047545c51cf33e64974c5d931acad
DimaKosy <dmytrokosynskyy@gmail.com>
2023-12-03 19:07:14 +0000

    rest

--- Nodes/node/app/model/select.js
+++ Nodes/node/app/model/select.js
@@ -3,8 +3,8 @@ var mysql = require('mysql2');
 var db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "seriousSql1",
-    database: "mydb",
+    password: "1234",
+    database: "sys",
     port: 3306
 });
 
