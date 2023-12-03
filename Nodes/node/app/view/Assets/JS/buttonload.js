1f3c721575ce059f828580e971e97e9a3666bc9b
Darren Grants <darrengrants06@gmail.com>
2023-12-03 20:12:27 +0000

    working titles

--- Nodes/node/app/view/Assets/JS/buttonload.js
+++ Nodes/node/app/view/Assets/JS/buttonload.js
@@ -5,32 +5,31 @@ function loadMore() {
     buttonId++;
     document.getElementById('load-more-button').innerText = `Load More~! ♡ (${buttonId})`;
 
-    // Create and append three new grids
+    // Create and append new grids based on the data
     const gridContainer = document.getElementById('grid-container');
     const newRow = document.createElement('div');
     newRow.className = 'row';
+
+    // Use template literals to construct the correct URL
+    $.get(`/allgames/${buttonId++}`, function (data, status) {
+        data.forEach(function (game, i) { // Add 'i' as the second parameter
+            const newGrid1 = document.createElement('div');
+            newGrid1.className = 'col-lg-4';
+            newGrid1.innerHTML = `
+                <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="Assets/Images/New_Image_${buttonId * 3 + i}.avif" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>New Placeholder</title>
+                <rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
+                </img>
+                <h2 class="fw-normal"> ${game.game_name}</h2>
+                <p>New Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, omnis, soluta expedita maxime laboriosam cumque voluptatem minus porro eos labore itaque possimus a facere aut praesentium quod necessitatibus! Repellendus, alias.</p>
+                <p><a class="btn btn-secondary" href="#">View details »</a></p>
+            `;
+            newRow.appendChild(newGrid1);
+        });
     
-    $.get(`/allgames/${buttonId++}`,function(data,status){
-        for(var i = 1; i < data.length;i++){
-            console.log(data[i]);
-        }
+        // Append the new row to the grid container
+        gridContainer.appendChild(newRow);
     });
-
-
     
-    // for (let i = 1; i <= 3; i++) {
-    //     const newGrid = document.createElement('div');
-    //     newGrid.className = 'col-lg-4';
-    //     newGrid.innerHTML = `
-    //                 <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="Assets/Images/New_Image_${buttonId * 3 + i}.avif" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
-    //                 <title>New Placeholder</title>
-    //                 <rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
-    //                 </img>
-    //                 <h2 class="fw-normal">New Title ${buttonId * 3 + i}</h2>
-    //                 <p>New Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, omnis, soluta expedita maxime laboriosam cumque voluptatem minus porro eos labore itaque possimus a facere aut praesentium quod necessitatibus! Repellendus, alias.</p>
-    //                 <p><a class="btn btn-secondary" href="#">View details »</a></p>
-    //             `;
-    //     newRow.appendChild(newGrid);
-    // }
-    // gridContainer.appendChild(newRow);
 }
+
