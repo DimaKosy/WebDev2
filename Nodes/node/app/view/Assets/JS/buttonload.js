647d275e701599f7a06a00d294e1c742bf309bbd
Darren Grants <darrengrants06@gmail.com>
2023-12-03 19:03:34 +0000

    dima Addgrids.changes

--- Nodes/node/app/view/Assets/JS/buttonload.js
+++ Nodes/node/app/view/Assets/JS/buttonload.js
@@ -2,18 +2,22 @@ let buttonId = 0; // Initial button id
 
 function loadMore() {
     // Increment button id and update the button text
-    buttonId++;
+    buttonId;
     document.getElementById('load-more-button').innerText = `Load More~! ♡ (${buttonId})`;
 
     // Create and append three new grids
     const gridContainer = document.getElementById('grid-container');
     const newRow = document.createElement('div');
     newRow.className = 'row';
+    
+    $.get("/allgames/$(buttonId)",function(data,status){
+        data.forEach(function(game)
+        {
+            const newGrid1 = document.createElement('div');
+            newRow.appendChild(newGrid1);
 
-    for (let i = 1; i <= 3; i++) {
-        const newGrid = document.createElement('div');
-        newGrid.className = 'col-lg-4';
-        newGrid.innerHTML = `
+            newGrid1.className = 'col-lg-4';
+            newGrid1.innerHTML = `
                     <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="Assets/Images/New_Image_${buttonId * 3 + i}.avif" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
                     <title>New Placeholder</title>
                     <rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
@@ -22,7 +26,23 @@ function loadMore() {
                     <p>New Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, omnis, soluta expedita maxime laboriosam cumque voluptatem minus porro eos labore itaque possimus a facere aut praesentium quod necessitatibus! Repellendus, alias.</p>
                     <p><a class="btn btn-secondary" href="#">View details »</a></p>
                 `;
-        newRow.appendChild(newGrid);
-    }
-    gridContainer.appendChild(newRow);
+                newRow.appendChild(newGrid1);
+        });
+    });
+    
+    // for (let i = 1; i <= 3; i++) {
+    //     const newGrid = document.createElement('div');
+    //     newGrid.className = 'col-lg-4';
+    //     newGrid.innerHTML = `
+    //                 <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="Assets/Images/New_Image_${buttonId * 3 + i}.avif" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
+    //                 <title>New Placeholder</title>
+    //                 <rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
+    //                 </img>
+    //                 <h2 class="fw-normal">New Title ${buttonId * 3 + i}</h2>
+    //                 <p>New Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, omnis, soluta expedita maxime laboriosam cumque voluptatem minus porro eos labore itaque possimus a facere aut praesentium quod necessitatibus! Repellendus, alias.</p>
+    //                 <p><a class="btn btn-secondary" href="#">View details »</a></p>
+    //             `;
+    //     newRow.appendChild(newGrid);
+    // }
+    // gridContainer.appendChild(newRow);
 }
